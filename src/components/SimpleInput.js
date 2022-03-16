1f6ebfa293f5037791b6c5818f4564f7406c9992
Mohamed Hossam <m.hossameldine@gmail.com>
2022-03-16 23:20:07 +0200

    handling input validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,53 @@
+import { useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim().length > 6;
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const enteredNameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = () => {
+    setEnteredNameTouched(true);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+
+    setEnteredName('');
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={submitHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          onChange={enteredNameChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className='error-text'>Name input is empty!</p>
+        )}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
